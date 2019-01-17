a0d4c49617fc82b3ae4a612786094a57f5e1697a
Daniel Kraft <d@domob.eu>
2019-01-17 16:30:58 +0100

    Include movement in character JSON.
    Add fields for the partial step, waypoints and precomputed steps in the
    JSON of characters in the game state.

--- src/gamestatejson.cpp
+++ src/gamestatejson.cpp
@@ -1,6 +1,10 @@
 #include "gamestatejson.hpp"
 
 #include "jsonutils.hpp"
+#include "protoutils.hpp"
+
+#include "hexagonal/pathfinder.hpp"
+#include "proto/character.pb.h"
 
 namespace pxd
 {
@@ -8,11 +12,55 @@ namespace pxd
 Json::Value
 CharacterToJson (const Character& c)
 {
+  const auto& pb = c.GetProto ();
+
   Json::Value res(Json::objectValue);
   res["id"] = static_cast<int> (c.GetId ());
   res["owner"] = c.GetOwner ();
   res["name"] = c.GetName ();
   res["position"] = CoordToJson (c.GetPosition ());
+
+  Json::Value mv(Json::objectValue);
+  if (c.GetPartialStep () != 0)
+    mv["partialstep"] = static_cast<int> (c.GetPartialStep ());
+  if (pb.has_movement ())
+    {
+      const auto& mvProto = pb.movement ();
+
+      Json::Value wp(Json::arrayValue);
+      for (const auto& entry : mvProto.waypoints ())
+        wp.append (CoordToJson (CoordFromProto (entry)));
+      if (wp.size () > 0)
+        mv["waypoints"] = wp;
+
+      /* The precomputed path is processed (rather than just translated from
+         proto to JSON):  We strip off already visited points from it, and
+         we "shift" it by one so that the points represent destinations
+         and it is easier to understand.  */
+      Json::Value path(Json::arrayValue);
+      bool foundPosition = false;
+      for (const auto& s : mvProto.steps ())
+        {
+          const HexCoord from = CoordFromProto (s);
+          if (from == c.GetPosition ())
+            {
+              CHECK (!foundPosition);
+              foundPosition = true;
+            }
+          else if (foundPosition)
+            path.append (CoordToJson (from));
+        }
+      CHECK (foundPosition || mvProto.steps_size () == 0);
+      if (foundPosition)
+        {
+          CHECK (wp.size () > 0);
+          path.append (wp[0]);
+          mv["steps"] = path;
+        }
+    }
+  if (mv.size () > 0)
+    res["movement"] = mv;
+
   return res;
 }
 
--- src/gamestatejson_tests.cpp
+++ src/gamestatejson_tests.cpp
@@ -1,6 +1,9 @@
 #include "gamestatejson.hpp"
 
+#include "protoutils.hpp"
+
 #include "database/character.hpp"
+#include "proto/character.pb.h"
 
 #include "database/dbtest.hpp"
 
@@ -34,22 +37,34 @@ protected:
     in >> expected;
 
     const Json::Value actual = GameStateToJson (*db);
+    VLOG (1) << "Actual JSON for the game state:\n" << actual;
     ASSERT_EQ (actual, expected);
   }
 
 };
 
-TEST_F (GameStateJsonTests, Characters)
-{
-  CharacterTable tbl(*db);
+/* ************************************************************************** */
 
+class CharacterJsonTests : public GameStateJsonTests
+{
+
+protected:
+
+  CharacterTable tbl;
+
+  CharacterJsonTests ()
+    : tbl(*db)
+  {}
+
+};
+
+TEST_F (CharacterJsonTests, Basic)
+{
   auto c = tbl.CreateNew ("domob", "foo");
   c->SetPosition (HexCoord (-5, 2));
   c.reset ();
 
-  /*
   tbl.CreateNew ("andy", u8"äöü");
-  */
 
   ExpectStateJson (u8R"({
     "characters":
@@ -57,10 +72,123 @@ TEST_F (GameStateJsonTests, Characters)
         {
           "id": 1, "name": "foo", "owner": "domob",
           "position": {"x": -5, "y": 2}
+        },
+        {
+          "id": 2, "name": "äöü", "owner": "andy",
+          "position": {"x": -0, "y": 0}
         }
       ]
   })");
 }
+
+TEST_F (CharacterJsonTests, Waypoints)
+{
+  auto c = tbl.CreateNew ("domob", "foo");
+  c->SetPartialStep (5);
+  auto* wp = c->MutableProto ().mutable_movement ()->mutable_waypoints ();
+  *wp->Add () = CoordToProto (HexCoord (-3, 0));
+  *wp->Add () = CoordToProto (HexCoord (0, 42));
+  c.reset ();
+
+  ExpectStateJson (R"({
+    "characters":
+      [
+        {
+          "id": 1, "name": "foo", "owner": "domob",
+          "position": {"x": 0, "y": 0},
+          "movement":
+            {
+              "partialstep": 5,
+              "waypoints": [{"x": -3, "y": 0}, {"x": 0, "y": 42}]
+            }
+        }
+      ]
+  })");
+}
+
+TEST_F (CharacterJsonTests, OnlyOneStep)
+{
+  auto c = tbl.CreateNew ("domob", "foo");
+  c->SetPosition (HexCoord (2, 3));
+  auto* mvProto = c->MutableProto ().mutable_movement ();
+  *mvProto->mutable_waypoints ()->Add () = CoordToProto (HexCoord (42, -42));
+  *mvProto->mutable_steps ()->Add () = CoordToProto (HexCoord (2, 3));
+  c.reset ();
+
+  ExpectStateJson (R"({
+    "characters":
+      [
+        {
+          "id": 1, "name": "foo", "owner": "domob",
+          "position": {"x": 2, "y": 3},
+          "movement":
+            {
+              "waypoints": [{"x": 42, "y": -42}],
+              "steps": [{"x": 42, "y": -42}]
+            }
+        }
+      ]
+  })");
+}
+
+TEST_F (CharacterJsonTests, PositionIsLastStep)
+{
+  auto c = tbl.CreateNew ("domob", "foo");
+  c->SetPosition (HexCoord (2, 3));
+  auto* mvProto = c->MutableProto ().mutable_movement ();
+  *mvProto->mutable_waypoints ()->Add () = CoordToProto (HexCoord (42, -42));
+  *mvProto->mutable_steps ()->Add () = CoordToProto (HexCoord (-5, 10));
+  *mvProto->mutable_steps ()->Add () = CoordToProto (HexCoord (2, 3));
+  c.reset ();
+
+  ExpectStateJson (R"({
+    "characters":
+      [
+        {
+          "id": 1, "name": "foo", "owner": "domob",
+          "position": {"x": 2, "y": 3},
+          "movement":
+            {
+              "waypoints": [{"x": 42, "y": -42}],
+              "steps": [{"x": 42, "y": -42}]
+            }
+        }
+      ]
+  })");
+}
+
+TEST_F (CharacterJsonTests, MultipleStep)
+{
+  auto c = tbl.CreateNew ("domob", "foo");
+  c->SetPosition (HexCoord (2, 3));
+  auto* mvProto = c->MutableProto ().mutable_movement ();
+  *mvProto->mutable_waypoints ()->Add () = CoordToProto (HexCoord (42, -42));
+  *mvProto->mutable_steps ()->Add () = CoordToProto (HexCoord (-5, 10));
+  *mvProto->mutable_steps ()->Add () = CoordToProto (HexCoord (2, 3));
+  *mvProto->mutable_steps ()->Add () = CoordToProto (HexCoord (7, 8));
+  c.reset ();
+
+  ExpectStateJson (R"({
+    "characters":
+      [
+        {
+          "id": 1, "name": "foo", "owner": "domob",
+          "position": {"x": 2, "y": 3},
+          "movement":
+            {
+              "waypoints": [{"x": 42, "y": -42}],
+              "steps":
+                [
+                  {"x": 7, "y": 8},
+                  {"x": 42, "y": -42}
+                ]
+            }
+        }
+      ]
+  })");
+}
+
+/* ************************************************************************** */
 
 } // anonymous namespace
 } // namespace pxd
