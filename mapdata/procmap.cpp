be6be2acba4109154721d797a678a3f15ad9ab39
Daniel Kraft <d@domob.eu>
2019-07-04 07:56:20 +0200

    Put code under GPL v3.
    Add the GPL v3 as license for the code in COPYING, and add the
    corresponding header comments to all relevant source files.
    
    Fixes https://github.com/xaya/taurion_gsp/issues/16.

--- mapdata/procmap.cpp
+++ mapdata/procmap.cpp
@@ -1,3 +1,21 @@
+/*
+    GSP for the Taurion blockchain game
+    Copyright (C) 2019  Autonomous Worlds Ltd
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* Utility program to read the raw map data files and convert them into a
    format that is compact and easy to read at runtime.  The processed data
    is then used by the BaseMap class.  */
