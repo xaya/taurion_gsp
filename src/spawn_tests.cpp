8262002cf07087dc6d0d1dce553f46a4b94367bc
Daniel Kraft <d@domob.eu>
2019-11-25 12:03:59 +0100

    Adjust character stats per faction.
    Set adjusted character stats, which are differentiated between the
    factions.  These will be the ones we use for the second competition
    (unless testing shows we need more tweaks), and have been designed by
    Andy.
    
    Note that they make some of the integration tests fail, which will be
    adjusted in a follow-up.

--- src/spawn_tests.cpp
+++ src/spawn_tests.cpp
@@ -97,7 +97,26 @@ TEST_F (SpawnTests, DataInitialised)
   ASSERT_EQ (c->GetOwner (), "domob");
 
   EXPECT_TRUE (c->GetProto ().has_combat_data ());
-  EXPECT_EQ (c->GetProto ().combat_data ().attacks_size (), 2);
+  EXPECT_FALSE (c->GetProto ().combat_data ().attacks ().empty ());
+}
+
+TEST_F (SpawnTests, PerFactionStats)
+{
+  const auto idRed = Spawn ("red", Faction::RED)->GetId ();
+  const auto idGreen = Spawn ("green", Faction::GREEN)->GetId ();
+  const auto idBlue = Spawn ("blue", Faction::BLUE)->GetId ();
+
+  auto c = tbl.GetById (idRed);
+  ASSERT_EQ (c->GetFaction (), Faction::RED);
+  EXPECT_EQ (c->GetProto ().speed (), 2'100);
+
+  c = tbl.GetById (idGreen);
+  ASSERT_EQ (c->GetFaction (), Faction::GREEN);
+  EXPECT_EQ (c->GetProto ().speed (), 2'000);
+
+  c = tbl.GetById (idBlue);
+  ASSERT_EQ (c->GetFaction (), Faction::BLUE);
+  EXPECT_EQ (c->GetProto ().speed (), 2'200);
 }
 
 class SpawnLocationTests : public SpawnTests
