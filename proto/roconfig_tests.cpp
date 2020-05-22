2feedafe5f061b50f5e34ea045eb13fc857b35e0
Daniel Kraft <d@domob.eu>
2020-05-22 13:28:07 +0200

    Sanity check for roconfig data.
    Add a unit test that sanity-checks the roconfig data for some things.
    Mostly this verifies the various cross-references of item types for
    validity (e.g. what resources something needs for construction or refines
    into).

--- proto/roconfig_tests.cpp
+++ proto/roconfig_tests.cpp
@@ -18,12 +18,17 @@
 
 #include "roconfig.hpp"
 
+#include "roitems.hpp"
+
+#include <glog/logging.h>
 #include <gtest/gtest.h>
 
 namespace pxd
 {
 namespace
 {
+
+/* ************************************************************************** */
 
 TEST (RoConfigTests, Parses)
 {
@@ -41,6 +46,180 @@ TEST (RoConfigTests, HasData)
 {
   EXPECT_GT (RoConfigData ().fungible_items ().size (), 0);
 }
+
+/* ************************************************************************** */
+
+class RoConfigSanityTests : public testing::Test
+{
+
+private:
+
+  /**
+   * Checks if all keys in the given map (e.g. item types to amounts
+   * needed for construction, or item types to amounts yielded from
+   * refining) are valid materials.
+   */
+  template <typename Map>
+    static bool
+    IsValidMaterialMap (const Map& m)
+  {
+    for (const auto& entry : m)
+      {
+        const std::string type = entry.first;
+        const auto* item = RoItemDataOrNull (type);
+        if (item == nullptr)
+          {
+            LOG (WARNING) << "Unknown item: " << type;
+            return false;
+          }
+
+        if (item->with_blueprint ()
+              || !item->construction_resources ().empty ()
+              || item->type_case () != proto::ItemData::TYPE_NOT_SET)
+          {
+            LOG (WARNING) << "Item is not a material: " << type;
+            return false;
+          }
+      }
+
+    return true;
+  }
+
+  /**
+   * Checks if the given item type is a valid raw material.
+   */
+  static bool
+  IsRawMaterial (const std::string& type)
+  {
+    const auto* item = RoItemDataOrNull (type);
+    if (item == nullptr)
+      {
+        LOG (WARNING) << "Unknown item: " << type;
+        return false;
+      }
+
+    /* FIXME: Once we actually have added in stats for refining of the
+       raw materials (raw X), check that the item type has a refines entry.  */
+    return true;
+  }
+
+  /**
+   * Checks if the given string is a valid equipment slot.
+   */
+  static bool
+  IsValidEquipmentSlot (const std::string& str)
+  {
+    return str == "high" || str == "mid" || str == "low";
+  }
+
+protected:
+
+  /**
+   * Checks if the given config instance is valid.  This verifies things
+   * like that item types referenced from other item configs (e.g. what
+   * something refines to) are actually valid.
+   */
+  static bool IsConfigValid (const proto::ConfigData& data);
+
+};
+
+bool
+RoConfigSanityTests::IsConfigValid (const proto::ConfigData& data)
+{
+  for (const auto& entry : data.fungible_items ())
+    {
+      const auto& i = entry.second;
+
+      if (!IsValidMaterialMap (i.construction_resources ()))
+        {
+          LOG (WARNING)
+              << "Item construction data is invalid for " << entry.first;
+          return false;
+        }
+
+      if (!IsValidMaterialMap (i.refines ().outputs ()))
+        {
+          LOG (WARNING) << "Refines-to data is invalid for " << entry.first;
+          return false;
+        }
+
+      if (i.has_is_blueprint ())
+        {
+          LOG (WARNING) << "Blueprint item defined: " << entry.first;
+          return false;
+        }
+
+      for (const auto& output : i.reveng ().possible_outputs ())
+        {
+          const auto* o = RoItemDataOrNull (output);
+          if (o == nullptr || !o->is_blueprint ().original ())
+            {
+              LOG (WARNING)
+                  << "Item " << entry.first
+                  << " has reveng output " << output
+                  << " which is not a blueprint original";
+              return false;
+            }
+        }
+
+      for (const auto& s : i.vehicle ().equipment_slots ())
+        if (!IsValidEquipmentSlot (s.first))
+          {
+            LOG (WARNING)
+                << "Vehicle type " << entry.first
+                << " has invalid equipment slot " << s.first;
+            return false;
+          }
+
+      if (i.has_fitment () && !IsValidEquipmentSlot (i.fitment ().slot ()))
+        {
+          LOG (WARNING)
+              << "Fitment " << entry.first
+              << " uses invalid slot " << i.fitment ().slot ();
+          return false;
+        }
+    }
+
+  for (const auto& entry : data.building_types ())
+    {
+      const auto& b = entry.second;
+      if (!IsValidMaterialMap (b.construction ().foundation ()))
+        {
+          LOG (WARNING)
+              << "Building foundation data is invalid for " << entry.first;
+          return false;
+        }
+      if (!IsValidMaterialMap (b.construction ().full_building ()))
+        {
+          LOG (WARNING)
+              << "Building construction data is invalid for " << entry.first;
+          return false;
+        }
+    }
+
+  for (const auto& entry : data.resource_dist ().base_amounts ())
+    if (!IsRawMaterial (entry.first))
+      {
+        LOG (WARNING) << "Invalid base amounts in resource dist";
+        return false;
+      }
+  for (const auto& area : data.resource_dist ().areas ())
+    for (const auto& type : area.resources ())
+      if (!IsRawMaterial (type))
+        {
+          LOG (WARNING) << "Invalid resource areas for distribution";
+          return false;
+        }
+
+  return true;
+}
+
+TEST_F (RoConfigSanityTests, Valid)
+{
+  EXPECT_TRUE (IsConfigValid (RoConfigData ()));
+}
+
+/* ************************************************************************** */
 
 } // anonymous namespace
 } // namespace pxd
