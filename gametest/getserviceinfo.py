6e15e38ec533f0e5a2683de2c9e05cfabe43ee7b
Daniel Kraft <d@domob.eu>
2020-05-26 11:00:10 +0200

    Integration test for getserviceinfo.
    The new integration test verifies the expected behaviour of the
    new RPC method getserviceinfo in all success and error paths.

---
+++ gametest/getserviceinfo.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+#   GSP for the Taurion blockchain game
+#   Copyright (C) 2020  Autonomous Worlds Ltd
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from pxtest import PXTest
+
+"""
+Tests the getserviceinfo RPC command.
+"""
+
+
+class GetServiceInfoTest (PXTest):
+
+  def run (self):
+    self.collectPremine ()
+
+    def getserviceinfo (name, op):
+      return self.getRpc ("getserviceinfo", name=name, op=op)
+
+    self.mainLogger.info ("Setting up basic situation...")
+    self.initAccount ("domob", "r")
+    self.initAccount ("andy", "r")
+    self.generate (1)
+
+    self.build ("ancient1", "domob", {"x": 0, "y": 0}, 0)
+    building = 1001
+    self.assertEqual (self.getBuildings ()[building].getOwner (), "domob")
+
+    self.getBuildings ()[building].sendMove ({"sf": 50})
+    self.generate (1)
+    self.assertEqual (self.getBuildings ()[building].data["servicefee"], 50)
+
+    self.dropIntoBuilding (building, "andy", {"test ore": 3})
+
+    self.mainLogger.info ("Testing invalid account...")
+    self.expectError (-2, "account does not exist",
+                      getserviceinfo, "invalid", {})
+
+    self.mainLogger.info ("Testing fully invalid service...")
+    self.assertEqual (getserviceinfo ("andy", {"foo": "bar"}), None)
+
+    self.mainLogger.info ("Testing validly parsed service...")
+    op = {"b": building, "t": "ref", "i": "test ore", "n": 3}
+    self.assertEqual (getserviceinfo ("andy", op), {
+      "type": "refining",
+      "building": building,
+      "input": {"test ore": 3},
+      "output": {"bar": 2, "zerospace": 1},
+      "cost": {"base": 10, "fee": 5},
+      "valid": False,
+    })
+    self.giftCoins ({"andy": 100})
+    self.assertEqual (getserviceinfo ("andy", op)["valid"], True)
+
+
+if __name__ == "__main__":
+  GetServiceInfoTest ().main ()
