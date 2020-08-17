35b92e72462711fd0518bb402d8b94e8ec8ae109
Daniel Kraft <d@domob.eu>
2020-08-17 11:49:52 +0200

    Support vehicles/fitments in stresstest script.
    This extends the old contrib/stresstest/create_characters.py script
    to allow specifying vehicle type and fitments for the created characters.

--- contrib/stresstest/create_characters.py
+++ contrib/stresstest/create_characters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #   GSP for the Taurion blockchain game
-#   Copyright (C) 2019  Autonomous Worlds Ltd
+#   Copyright (C) 2019-2020  Autonomous Worlds Ltd
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -26,25 +26,33 @@ import utils
 import jsonrpclib
 
 import argparse
-import json
+import collections
 import sys
 
 DEV_ADDRESS = "dHNvNaqcD7XPDnoRjAoyfcMpHRi5upJD7p"
-COST = 5
+COST = 1
 
 desc = "Creates Taurion characters"
 parser = argparse.ArgumentParser (description=desc)
 parser.add_argument ("--xaya_rpc_url", required=True,
                      help="JSON-RPC interface of regtest Xaya Core")
+parser.add_argument ("--gsp_rpc_url", required=True,
+                     help="JSON-RPC interface of the Taurion GSP")
 parser.add_argument ("--faction", required=True,
                      help="faction for created characters ('r', 'g' or 'b')")
-parser.add_argument ("--name", required=True,
-                     help="Xaya name that is used to create characters")
+parser.add_argument ("--vehicle", default=None,
+                     help="vehicle type to change into (if set)")
+parser.add_argument ("--fitments", default="",
+                     help="semicolon-separated list of fitments to apply")
+parser.add_argument ("--name_template", required=True,
+                     help="printf template for the names that will be used")
 parser.add_argument ("--count", type=int, required=True,
                      help="number of characters to create")
-parser.add_argument ("--per_block", type=int, default=100,
-                     help="how many characters to create per block")
+parser.add_argument ("--batch_size", type=int, default=20,
+                     help="number of characters to create per name")
 args = parser.parse_args ()
+
+################################################################################
 
 logger = utils.setupLogging ()
 
@@ -53,41 +61,154 @@ if args.faction not in ["r", "g", "b"]:
   sys.exit (1)
 
 rpc = utils.connectRegtestRpc (args.xaya_rpc_url, logger)
+gsp = utils.connectGspRpc (args.gsp_rpc_url, logger)
 
-fullname = "p/" + args.name
-try:
-  data = rpc.name_show (fullname)
-  if not data["ismine"]:
-    logger.fatal ("Name is not owned by wallet: %s" % fullname)
+buildings = utils.readGsp (rpc, logger, gsp.getbuildings)
+bId = None
+for b in buildings:
+  if b["type"] == "%s ss" % args.faction:
+    bId = b["id"]
+    break
+
+assert bId is not None
+logger.info ("Using building ID %d for vehicle / fitment changes" % bId)
+
+names = []
+for i in range ((args.count + args.batch_size - 1) // args.batch_size):
+  names.append (args.name_template % i)
+
+def getAccount (name):
+  """
+  Returns the account data for the given name, or None if there is none.
+  """
+
+  accounts = utils.readGsp (rpc, logger, gsp.getaccounts)
+
+  for a in accounts:
+    if a["name"] == name:
+      return a
+
+  return None
+
+for nm in names:
+  if getAccount (nm) is None:
+    logger.info ("Initialising account %s for faction %s..."
+                    % (nm, args.faction))
+    utils.sendMove (rpc, logger, nm, {"a": {"init": {"faction": args.faction}}})
+
+if rpc.getrawmempool ():
+  utils.mineBlocks (rpc)
+
+existingCharOwners = set ()
+for c in utils.readGsp (rpc, logger, gsp.getcharacters):
+  existingCharOwners.add (c["owner"])
+
+for nm in names:
+  if nm in existingCharOwners:
+    logger.fatal ("Account %s already has characters" % nm)
     sys.exit (1)
-except jsonrpclib.jsonrpc.ProtocolError:
-  logger.fatal ("Name does not exist: %s" % fullname)
-  sys.exit (1)
 
-logger.info ("Creating %d characters for %s..." % (args.count, args.name))
+  acc = getAccount (nm)
+  assert acc is not None
+  if acc["faction"] != args.faction:
+    logger.fatal ("%s is faction %s" % (nm, acc["faction"]))
+    sys.exit (1)
+  logger.debug ("%s is initialised to faction %s" % (nm, acc["faction"]))
+
+items = collections.Counter ()
+if args.vehicle is not None:
+  items[args.vehicle] += 1
+if args.fitments:
+  fitments = args.fitments.split (";")
+  for f in fitments:
+    items[f] += 1
+else:
+  fitments = []
+
+################################################################################
+
+logger.info ("Creating %d characters..." % args.count)
+
 done = 0
 while done < args.count:
-  now = min (args.per_block, args.count - done)
+  now = min (args.batch_size, args.count - done)
+  done += now
+  percent = (done * 100) // args.count
 
-  mv = {
-    "g":
-      {
-        "tn":
-          {
-            "nc": [{"faction": args.faction}] * now,
-          },
-      },
-  }
-  mvStr = json.dumps (mv, separators=(",", ":"))
+  assert names
+  nm = names[0]
+  names = names[1:]
+  logger.info ("Processing %d characters with %s (%d%% done)..."
+                  % (now, nm, percent))
 
+  # Create the characters.
+  mv = {"nc": [{}] * now}
   options = {
     "sendCoins":
       {
         DEV_ADDRESS: COST * now,
       },
   }
-  rpc.name_update (fullname, mvStr, options)
-  rpc.generatetoaddress (1, rpc.getnewaddress ())
+  utils.sendMove (rpc, logger, nm, mv, options)
 
-  done += now
-  logger.info ("Processed %d / %d characters" % (done, args.count))
+  # God-mode drop required items.
+  fungible = {}
+  for itm, cnt in items.items ():
+    fungible[itm] = cnt * now
+  cmd = {
+    "god":
+      {
+        "drop":
+          [
+            {
+              "building": {"id": bId, "a": nm},
+              "fungible": fungible,
+            },
+          ],
+      },
+  }
+  utils.adminCommand (rpc, logger, cmd)
+  utils.mineBlocks (rpc)
+
+  # Enter the building with all characters.
+  chars = utils.readGsp (rpc, logger, gsp.getcharacters)
+  cIds = []
+  for c in chars:
+    if c["owner"] == nm:
+      cIds.append (c["id"])
+  assert len (cIds) == now, "%d != %d" % (len (cIds), now)
+  cIdsStr = ",".join (["%d" % id for id in cIds])
+  mv = {"c": {cIdsStr: {"eb": bId}}}
+  utils.sendMove (rpc, logger, nm, mv)
+  utils.mineBlocks (rpc)
+
+  # Change vehicle, fitments, and exit building.
+  chars = utils.readGsp (rpc, logger, gsp.getcharacters)
+  for c in chars:
+    if c["owner"] != nm:
+      continue
+    if "inbuilding" not in c or c["inbuilding"] != bId:
+      logger.fatal ("Failed to enter building %d with character %d"
+                        % (bId, c["id"]))
+      sys.exit (1)
+  upd = {"fit": fitments, "xb": {}}
+  if args.vehicle is not None:
+    upd["v"] = args.vehicle
+  mv = {"c": {cIdsStr: upd}}
+  utils.sendMove (rpc, logger, nm, mv)
+  utils.mineBlocks (rpc)
+
+  # Verify all worked.
+  chars = utils.readGsp (rpc, logger, gsp.getcharacters)
+  for c in chars:
+    if c["owner"] != nm:
+      continue
+    if "inbuilding" in c:
+      logger.fatal ("Failed to exit building with character %d" % c["id"])
+      sys.exit (1)
+    if args.vehicle is not None and c["vehicle"] != args.vehicle:
+      logger.fatal ("Failed to change vehicle with character %d" % c["id"])
+      sys.exit (1)
+    if fitments != c["fitments"]:
+      logger.fatal ("Failed to change fitments with character %d" % c["id"])
+      sys.exit (1)
--- contrib/stresstest/utils.py
+++ contrib/stresstest/utils.py
@@ -20,8 +20,10 @@ Some common utility code shared between the individual stress-test scripts.
 
 import jsonrpclib
 
+import json
 import logging
 import sys
+import time
 
 
 def setupLogging ():
@@ -53,3 +55,94 @@ def connectRegtestRpc (url, logger):
     sys.exit (1)
 
   return rpc
+
+
+def connectGspRpc (url, logger):
+  """
+  Opens an RPC connection to the Taurion GSP on the given url
+  and verifies that it is good and synced up.
+  """
+
+  rpc = jsonrpclib.ServerProxy (url)
+
+  while True:
+    state = rpc.getnullstate ()
+
+    if state["state"] == "up-to-date":
+      logger.info ("GSP is up-to-date at height %d" % state["height"])
+      return rpc
+
+    logger.warning ("GSP is %s, waiting to be up-to-date..." % state["state"])
+    time.sleep (1)
+
+
+def readGsp (rpc, logger, fcn, *args, **kwargs):
+  """
+  Calls an RPC method on the GSP, verifies that it matches the current
+  RPC tip (if not, waits and tries again), and returns the "data" field
+  on success.
+  """
+
+  blk = rpc.getbestblockhash ()
+
+  while True:
+    res = fcn (*args, **kwargs)
+
+    if res["state"] != "up-to-date" or res["blockhash"] != blk:
+      logger.warning ("GSP is not yet caught up, waiting...")
+      time.sleep (1)
+      continue
+
+    return res["data"]
+
+
+def updateOrRegister (rpc, logger, name, val, options={}):
+  """
+  Updates or registers the given name with the given value.
+  """
+
+  try:
+    data = rpc.name_show (name)
+    if not data["ismine"]:
+      logger.fatal ("Name is not owned by wallet: %s" % name)
+      sys.exit (1)
+    return rpc.name_update (name, val, options)
+
+  except jsonrpclib.jsonrpc.ProtocolError:
+    logger.info ("Name does not exist yet, creating: %s" % name)
+    return rpc.name_register (name, val, options)
+
+
+def sendMove (rpc, logger, name, data, options={}):
+  """
+  Sends a Taurion move for the given name (without p/ prefix) and
+  data.  Optionally adds some options to the name_update.
+
+  If the name does not exist yet, it will be registered.
+  """
+
+  fullname = "p/" + name
+
+  mv = {"g": {"tn": data}}
+  mvStr = json.dumps (mv, separators=(",", ":"))
+
+  updateOrRegister (rpc, logger, fullname, mvStr, options)
+
+
+def adminCommand (rpc, logger, data):
+  """
+  Sends an admin command.
+  """
+
+  mv = {"cmd": data}
+  mvStr = json.dumps (mv, separators=(",", ":"))
+
+  updateOrRegister (rpc, logger, "g/tn", mvStr)
+
+
+def mineBlocks (rpc, n=1):
+  """
+  Mines n blocks.
+  """
+
+  rpc.generatetoaddress (n, rpc.getnewaddress ())
