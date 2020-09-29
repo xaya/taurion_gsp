7ffb8150ccd68016c66a8c7a1b7995e2ea610090
Daniel Kraft <d@domob.eu>
2020-09-29 11:38:19 +0200

    Cache bootstrap data for REST API.
    Keep a copy of the bootstrap data for the REST API in memory, and
    only refresh it with a certain interval.  (If we are still catching up,
    no cache is kept.)

--- src/rest.hpp
+++ src/rest.hpp
@@ -10,6 +10,11 @@
 #include <xayagame/game.hpp>
 #include <xayagame/rest.hpp>
 
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <thread>
+
 namespace pxd
 {
 
@@ -27,6 +32,34 @@ private:
   /** The game logic implementation.  */
   PXLogic& logic;
 
+  /**
+   * The current bootstrap data payload, if we have one cached.  This is a
+   * shared pointer, so that we can create copies quickly (while holding the
+   * lock) and then release it again while we send the result to a client.
+   */
+  std::shared_ptr<SuccessResult> bootstrapData;
+
+  /** Lock for the bootstrap data cache.  */
+  std::mutex mutBootstrap;
+
+  /** Set to true if we should stop.  */
+  bool shouldStop;
+
+  /** Condition variable that is signaled if shouldStop is set.  */
+  std::condition_variable cvStop;
+
+  /** Mutex for the stop flag and condition variable.  */
+  std::mutex mutStop;
+
+  /** Thread running the bootstrap data update.  */
+  std::unique_ptr<std::thread> bootstrapRefresher;
+
+  /**
+   * Computes the bootstrap data and returns it.  This may fill in the
+   * cache (if we are up-to-date), but does not use an existing cache.
+   */
+  std::shared_ptr<SuccessResult> ComputeBootstrapData ();
+
 protected:
 
   SuccessResult Process (const std::string& url) override;
@@ -36,6 +69,9 @@ public:
   explicit RestApi (xaya::Game& g, PXLogic& l, const int p)
     : xaya::RestApi(p), game(g), logic(l)
   {}
+
+  void Start () override;
+  void Stop () override;
 
 };
 
